aae02adf1f61354ddbf26d7eb3a0e0b4688d9577
Altech Inferno <altechinferno@gmail.com>
2023-06-12 22:38:12 -0700

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    # Check if the input list is None
     if my_list is None:
         return
 
+    # Check if the index is out of bounds
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
 
+     # Construct a new list with the element replaced
     return my_list[:idx] + [element] + my_list[idx+1:]
 
