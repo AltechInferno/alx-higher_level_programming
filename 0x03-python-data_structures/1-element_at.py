2c927a01c2ebf5b2cb445a8aaf4f3ac35e242e74
Altech Inferno <altechinferno@gmail.com>
2023-06-12 22:24:31 -0700

    element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    try:
-        return my_list[idx]
-    except IndexError:
+    if idx < 0 or idx >= len(my_list):
         return None
 
+    return my_list[idx]
