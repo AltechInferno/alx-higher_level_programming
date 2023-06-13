fcc3b34e59dfdbbc61471835098a7ac1d5b2581f
Altech Inferno <altechinferno@gmail.com>
2023-06-13 12:56:55 -0700

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements from each tuple
-    a = tuple_a[:2]
-    b = tuple_b[:2]
-
-    # Pad tuples with zeros if smaller than 2 elements
-    a += (0,) * (2 - len(a))
-    b += (0,) * (2 - len(b))
-
-    # Perform element-wise addition
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
     result = (a[0] + b[0], a[1] + b[1])
-
     return result
 
