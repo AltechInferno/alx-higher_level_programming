4c1b91004a70438cdaef00d9846bc668c4b20268
Altech Inferno <altechinferno@gmail.com>
2023-06-13 19:49:52 -0700

    is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,48 +1,73 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL, *current = *head, *next = NULL;
-	while (next = current->next)
-	{
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	current->next = prev;
-	*head = current;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
+ * is_palindrome - this block checks if a linked list is a palindrome
+ * @head: this is a double pointer to the linked list
  *
  * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
 	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (fast && fast->next)
+
+	while (1)
 	{
 		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
 		slow = slow->next;
 	}
-	dup = slow->next;
+
 	reverse_listint(&dup);
+
 	while (dup && temp)
 	{
-		if (temp->n != dup->n)
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
 			return (0);
-		dup = dup->next;
-		temp = temp->next;
 	}
-	return (1);
+
+	if (!dup)
+		return (1);
+
+	return (0);
+}
+
+
+/**
+ * reverse_listint - this block reverses a linked list
+ * @head: a pointer to the first node in the list
+ *
+ * Return: a pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *previous = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
+	}
+
+	*head = previous;
 }
