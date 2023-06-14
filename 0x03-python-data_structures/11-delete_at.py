64e482f3829beda832f0e9a9ba1d3382206315c8
Altech Inferno <altechinferno@gmail.com>
2023-06-13 19:31:58 -0700

    delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    
-    return my_list[:idx] + my_list[idx+1:]
+def delete_at(list=[], idx=0):
+    if idx < 0 or idx >= len(list):
+        return (list)
+
+    if list is None:
+        return (None)
+
+    del list[idx]
+    return (list)
