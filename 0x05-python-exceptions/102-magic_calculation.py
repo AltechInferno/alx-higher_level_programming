9894ca7e64e56e6362a1758251b1575267d94c60
Altech Inferno <altechinferno@gmail.com>
2023-06-27 14:19:47 -0700

    magic cal

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+# magic-calculations
 
 def magic_calculation(x, y):
     result = 0
-    for index in range(1, 3):
+    for i in range(1, 3):
         try:
-            if index > x:
+            if i > x:
                 raise Exception('Too far')
             else:
-                result += x ** y / index
+                result += x ** y / i
         except:
             result = y + x
             break
