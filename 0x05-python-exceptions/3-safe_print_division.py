ad03c8aefcf3eae2dbad3df9fdd239f71e4d5982
Altech Inferno <altechinferno@gmail.com>
2023-06-27 13:20:48 -0700

    3-safe-print-division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#3 safe print division 
+
+def safe_print_division(x, y):
+    """Returns division of x by y."""
+    try:
+        div = x / y
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
