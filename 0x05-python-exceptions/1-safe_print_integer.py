53b874dcf3b14233283f2a8ae8967c9b8cef59c0
Altech Inferno <altechinferno@gmail.com>
2023-06-27 12:52:42 -0700

    1safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+def safe_print_integer(value):
+    """Print an integer using "{:d}".format().
+
+    Args:
+        value: The value to print.
+
+    Returns:
+        bool: True if value is an integer and printed successfully, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
+
