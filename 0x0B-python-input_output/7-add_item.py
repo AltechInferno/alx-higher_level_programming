6877a9204cad5b32cc9ccb1b3d8e50e358340aeb
Altech Inferno <altechinferno@gmail.com>
2023-07-13 01:50:25 +0000

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import json
+import os.path
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""
+7-add_item component
+"""
+file = "add_item.json"
+list = []
+
+if os.path.exists(file):
+    list = load_from_json_file(file)
+
+for j in range(1, len(sys.argv)):
+    list.append(sys.argv[j])
+
+save_to_json_file(list, file)
