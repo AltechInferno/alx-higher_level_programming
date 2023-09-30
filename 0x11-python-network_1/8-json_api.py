bd08041a1de96e1553da796df235babafc092dcc
Altech Inferno <altechinferno@gmail.com>
2023-09-30 02:16:01 +0000

    task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script takes in a letter and sends a post request
+to http://0.0.0.0:5000/search_user with letter param
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+
+    url = 'http://0.0.0.0:5000/search_user'
+    query = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {'q': query}
+    encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+    request = urllib.request.Request(url, data=encoded_data, method='POST')
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            x_request_id = response.headers.get('X-Request-Id')
+            response_content = response.read().decode('utf-8')
+
+        print(f'X-Request-Id: {x_request_id}')
+        print(response_content)
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
