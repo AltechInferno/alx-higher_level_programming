cf2268e39783962245a9d8ce12acee5314eae0ac
Altech Inferno <altechinferno@gmail.com>
2023-09-30 02:26:00 +0000

    task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,25 +2,19 @@
 """script takes in a letter and sends a post request
 to http://0.0.0.0:5000/search_user with letter param
 """
-
-import urllib.request
-import sys
-
 if __name__ == "__main__":
-
+    from requests import post
+    from sys import argv
+    
     url = 'http://0.0.0.0:5000/search_user'
-    query = sys.argv[1] if len(sys.argv) > 1 else ""
-    data = {'q': query}
-    encoded_data = urllib.parse.urlencode(data).encode('utf-8')
-    request = urllib.request.Request(url, data=encoded_data, method='POST')
-
+    query = argv[1] if len(argv) > 1 else ""
+    data={'q':query}
+    req = post(url, data)
     try:
-        with urllib.request.urlopen(request) as response:
-            x_request_id = response.headers.get('X-Request-Id')
-            response_content = response.read().decode('utf-8')
-
-        print('X-Request-Id:', {x_request_id})
-        print(response_content)
-    except Exception as e:
+        response = req.json()
+        if response == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print('Not a valid JSON')
-
