463878c6067fc97af33a3b4e5f367b9d99caca9b
Altech Inferno <altechinferno@gmail.com>
2023-09-30 00:57:57 +0000

    task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     import sys
     
     url = sys.argv[1]
-
     with urllib.request.urlopen(url) as response:
         x_request_id = response.headers.get('X-Request-Id')
         print(x_request_id)
